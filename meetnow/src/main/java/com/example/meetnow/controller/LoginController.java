6de68c1421966c27cf678d714df82d66792b55d1
kim hyun sun <aptlf153@naver.com>
2025-04-09 23:08:46 +0900

    카카오 구글 네이버 로그인 완료

--- meetnow/src/main/java/com/example/meetnow/controller/LoginController.java
+++ meetnow/src/main/java/com/example/meetnow/controller/LoginController.java
@@ -18,6 +18,8 @@ import com.example.meetnow.util.JwtUtil;
 import jakarta.servlet.http.HttpServletResponse;
 
 import com.example.meetnow.dto.*;
+import com.example.meetnow.repository.CookieUtil;
+
 import org.springframework.http.HttpHeaders;
 
 @RestController
@@ -30,11 +32,12 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 	
-
     @Autowired
     private JwtUtil jwtUtil; // JWT 유틸리티 클래스	
     
-
+    @Autowired
+    private CookieUtil cookieUtil;
+    
 	//로그인
     @PostMapping("/api/users/login")
     public ResponseEntity<Boolean> login(@RequestBody DTO data, HttpServletResponse response) {
@@ -52,25 +55,8 @@ public class LoginController {
             String sql = "INSERT INTO refresh_tokens (userid, refreshtoken,expiration_date,created_date) VALUES (?,?,?,?)";
             jdbcTemplate.update(sql, data.getUserid(), refreshToken, createdDate, expirationDate);
 
-            ResponseCookie accessCookie = ResponseCookie.from("accessToken", accessToken)
-                    .httpOnly(true) // 클라이언트에서 접근 불가
-                    .path("/")
-                    .maxAge(60) //10분
-                    .sameSite("Lax") // 같은 도메인에서만
-                    .secure(false) // 개발 환경에서는 false로 설정
-                    .domain("localhost") // 로컬 환경에서만 유효
-                    .build();
-            response.addHeader(HttpHeaders.SET_COOKIE, accessCookie.toString());
-            
-            ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
-                    .httpOnly(true) // 클라이언트에서 접근 불가
-                    .path("/")
-                    .maxAge(60 * 24 * 7)
-                    .sameSite("Lax") // 같은 도메인에서만
-                    .secure(false) // 개발 환경에서는 false로 설정
-                    .domain("localhost") // 로컬 환경에서만 유효
-                    .build();
-            response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());     
+            //쿠키 만들기 컴포넌트
+            cookieUtil.addCookies(response, accessToken, refreshToken);
 
             // 응답 본문 반환
             return ResponseEntity.ok(true);
